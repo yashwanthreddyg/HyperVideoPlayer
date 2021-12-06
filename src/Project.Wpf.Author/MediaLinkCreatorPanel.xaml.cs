c62afd164c42916b61969ce4547597c0f6cfd58d
Harsh Jani <whizkid1236@gmail.com>
2021-12-05 22:36:03 -0800

    Adding boilerplate for launching modal dialog

---
+++ src/Project.Wpf.Author/MediaLinkCreatorPanel.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Project.Wpf.Author
+{
+    /// <summary>
+    /// Interaction logic for MediaLinkCreatorPanel.xaml
+    /// </summary>
+    public partial class MediaLinkCreatorPanel : UserControl
+    {
+        public MediaLinkCreatorPanel()
+        {
+            InitializeComponent();
+
+            this.Width = 800;
+            this.Height = 474;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
